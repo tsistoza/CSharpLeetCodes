771512ba1df19e4ff84a84e8fd8300ca1736c8c2
Vinton Sistoza <tsistoza@gmail.com>
2023-08-04 21:59:30 -1000

    Rotate,InsertPOS CSharp

---
+++ Rotate/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        //ONLY CHECKD FOR 3x3 MATRIX
+        protected static void Rotate(int[,] matrix){
+            Console.WriteLine("\nRotated Matrix: ");
+            int l = 0;
+            int r = matrix.GetLength(1) - 1;
+            while(l<r){
+                for(int i=0 ; i<r-l ; i++){
+                    int top = l;
+                    int bottom = r;
+
+                    //Starting from the top left work around the square and save the element
+                    int topLeft = matrix[top,l+i];
+
+                    //move bottom left into top left
+                    matrix[top,l+i] = matrix[bottom-i,l];
+
+                    //move bottom right into bottom left
+                    matrix[bottom-i,l] = matrix[bottom,r-i];
+
+                    //move top right into bottom right
+                    matrix[bottom,r-i] = matrix[top+i,r];
+
+                    //move top left into top right
+                    matrix[top+i,r] = topLeft;
+                }
+                r--;
+                l++;
+            }
+            printArray(matrix);
+            return;
+        }
+        protected static void printArray(int[,] matrix){
+            for(int i=0 ; i<matrix.GetLength(0) ; i++){
+                for(int j=0 ; j<matrix.GetLength(1) ; j++){
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.Write("\n");
+            }
+            return;
+        }
+        public static void Main(string[] args){
+            Program soln = new Program();
+            int[ , ] matrix = new int [4,4] { {5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16} };
+            Console.WriteLine($"{matrix.GetLength(0)} Rows {matrix.GetLength(1)} Cols ");
+            Console.WriteLine("ORIGINAL ARRAY: ");
+            printArray(matrix);
+            Rotate(matrix);
+            return;
+        }
+    }
+}
---
+++ SearchInsertPos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Insertion
+    {
+        /*
+        Given a sorted array of distinct integers and a target value. return the index
+        if the target is found. If not, return the index where it would be if it were inserted
+        in order. -> O(logn)
+        */
+        public int SearchInsert(int[] nums, int target){
+            for(int i=0 ; i<nums.Length ; i++){
+                if(nums[i] == target)
+                    return i;
+                if(nums[i] < target && i+1 < nums.Length && nums[i+1] > target)
+                    return i+1;
+                if(i == nums.Length-1)
+                    return i+1;
+            }
+            return 0;
+        } //function
+
+    } //class
+} //namespace
+
+
+namespace mainN{
+    public class Program
+    {
+        public static void Main(string[] args){
+            //PARAMETERS
+            int[] nums = {1,3,5,6,7,8,9};
+            int target = 10;
+
+            Solution.Insertion soln = new Solution.Insertion();
+            int index = soln.SearchInsert(nums,target);
+            Console.WriteLine(index);
+        }//main
+    }//class
+}//main
