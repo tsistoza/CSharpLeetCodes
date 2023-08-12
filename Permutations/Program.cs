4f3cd88bfca8f26edbbdc4dd5d4ba03d1e6be86c
Vinton Sistoza <tsistoza@gmail.com>
2023-08-12 11:11:52 -1000

    Permutations,UniquePaths

---
+++ Permutations/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace solution
+{
+    public class Solution
+    {
+        public static void Permute(int[] nums) {
+            List<int> numsList = new List<int>(nums);
+
+            //Create List of Lists
+            List<List<int>> list = new List<List<int>>();
+
+            //Generate Permutations
+            foreach(List<int> sublist in helper(numsList))
+                list.Add(sublist);
+            
+            printList(list);
+        }
+
+        public static IEnumerable<List<int>> helper(List<int> sublist) { //Since we are returning multiple iterators use IEnumerable too iterate over them.
+            if(sublist.Count == 2){
+                yield return new List<int> (sublist);
+                yield return new List<int> {sublist[1],sublist[0]};
+            }
+            else{
+                foreach(int i in sublist){ //CHECK FOR ALL PERMUTATIONS OF i.
+                    var rlist = new List<int> (sublist);
+                    rlist.Remove(i);
+                    foreach(List<int> retList in helper(rlist) ){
+                        retList.Insert(0,i); 
+                        yield return retList;
+                    }
+                }
+            }
+        }
+
+        public static void printList(List<List<int>> list){
+            Console.WriteLine("The Permutations are: ");
+            int index=0;
+            foreach(List<int> value in list)
+            {
+                Console.WriteLine($"Permutation #{index}: ");
+                foreach(int item in value){
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+                index++;
+            }
+            return;
+        }
+
+        public static void Main(string[] args){
+            int[] nums = {1,2,3};
+            Permute(nums);
+            return;
+        }
+    }
+}
---
+++ UniquePaths/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace solution{
+    class Program
+    {
+        public static int findPaths(int[ , ] grid,int row, int col){
+            int sum=0;
+            Console.WriteLine($"Row: {row} Col: {col}");
+            if(grid[row,col] == 1){
+                return 0;
+            }
+
+            if(grid[row,col] == 0){
+                if (row<grid.GetLength(0)-1)
+                    sum += findPaths(grid,row+1,col);
+                if(col<grid.GetLength(1)-1)
+                    sum += findPaths(grid,row,col+1);
+            }
+
+            //IF YOU REACH THE END W/O HITTING AN OBJECT RETURN 1
+            if(row == grid.GetLength(0)-1 && col == grid.GetLength(1)-1)
+                return 1;
+
+            return sum;
+        }
+        public static void Main(string[] args){
+            List<List<int>> pathList = new List<List<int>>();
+            int[ , ] grid = { {0,1},{0,0} };
+            int numOfPaths = findPaths(grid,0,0);
+            Console.WriteLine($"The number of paths: {numOfPaths}");
+            return;
+        }
+    }
+
+}
