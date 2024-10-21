2748d4448421e7ee21e7cc237c1dbe5e5d4d6f39
Vinton Sistoza <tsistoza@gmail.com>
2024-10-21 10:10:54 -1000

    SubMax LeetCode 1593

---
+++ SubMax/SubMax.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static readonly string s = "aa";
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        private List<string> substrings = new List<string>();
+
+        public int MaxUniqueSplit(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            findSubstrings(charArray);
+            return this.substrings.Count;
+        }
+
+        private void readSubstrings(IEnumerable<string> list)
+        {
+            Console.WriteLine("Substrings:");
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+            return;
+        }
+
+        private void findSubstrings(char[] charArray)
+        {
+            string s = "";
+            for (int i=0; i<charArray.Length; i++)
+            {
+                s += charArray[i];
+                if (!this.substrings.Contains(s))
+                {
+                    this.substrings.Add(s);
+                    s = "";
+                }
+                if ( i == charArray.Length-1 && s != "")
+                {
+                    string lastString = this.substrings.Last();
+                    s = lastString + charArray[i];
+                    if (!this.substrings.Contains(s) && lastString != charArray[i].ToString())
+                    {
+                        this.substrings.Remove(lastString);
+                        this.substrings.Add(s);
+                    }
+                } 
+            }
+            readSubstrings(substrings);
+            return;
+        }
+
+
+        public static int Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine($"Max Number of Substrings is: {obj.MaxUniqueSplit(Globals.s)}");
+            return 0;
+        }
+    }
+}
