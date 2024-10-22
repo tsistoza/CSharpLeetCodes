abb250f0534d402bb2e708cd3d70ee1f83a61bd9
Vinton Sistoza <tsistoza@gmail.com>
2024-10-21 14:21:50 -1000

    InsertionSort 147

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+
+public static class Globals
+{
+    public static readonly List<int> nums = new List<int>() { 4, 2, 1, 3 };
+    public static readonly List<int> nums2 = new List<int>() { -1, 5, 3, 4, 0 };
+}
+
+
+namespace Solution
+{
+    public class Program
+    {
+        public LinkedList<int> list = new LinkedList<int>();
+
+        public void readLinkedList()
+        {
+            Console.Write("Linked List: ");
+            foreach(int i in this.list)
+            {
+                Console.Write($"{i} -> ");
+            }
+            Console.Write("NULL\n");
+            return;
+        }
+        public void InsertNode(int value)
+        {
+            // List has no nodes
+            if (this.list.Count == 0)
+            {
+                this.list.AddLast(value);
+                return;
+            }
+
+            // Sort Nodes based on values
+            IEnumerator<int> enumerator = this.list.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                if (value < enumerator.Current)
+                {
+                    LinkedListNode<int> node = this.list.Find(enumerator.Current)!;
+                    this.list.AddBefore(node, value);
+                    return;
+                }
+            }
+
+            // If it did not return, then there is no value in 'this.list' that is < given passed value, so we just append value to end of list.
+            this.list.AddLast(value);
+            return;
+        }
+
+        public static int Main()
+        {
+            Program obj = new Program();
+            foreach (int i in Globals.nums) obj.InsertNode(i);
+            obj.readLinkedList();
+            obj.list.Clear();
+            foreach (int i in Globals.nums2) obj.InsertNode(i);
+            obj.readLinkedList();
+            obj.list.Clear();
+            return 0;
+        }
+    }
+}
