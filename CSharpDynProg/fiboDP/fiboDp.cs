ec756ac530b0361f57c00a33d01de8842f503371
Vinton Sistoza <tsistoza@gmail.com>
2024-11-01 11:24:55 -1000

    fiboDP.cpp
    fibonacci Dynamic Programming

---
+++ CSharpDynProg/fiboDP/fiboDp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program
+    {
+        public static int fibonacii(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+            for(int i=3; i<=n; i++)
+                dp[i] = dp[i - 1] + dp[i - 2];
+            return dp[n];
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(fibonacii(3));
+            return;
+        }
+    }
+}
