58c0108ebd39a3b2161f33e0b6d8e61c87e098c7
Vinton Sistoza <tsistoza@gmail.com>
2024-11-16 10:26:29 -1000

    BSTPostOrder.cs 145
    LeetCode 145

---
+++ BSTPostOrder/BSTPostOrder.cs
@@ -0,0 +1,105 @@
+﻿// LeetCode 145
+using System;
+using System.Collections;
+
+namespace Solution
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int val = 0, TreeNode? left=null, TreeNode? right=null)
+        {
+            this.val = val; this.left = left; this.right = right;
+        }
+    }
+
+    public class BST
+    {
+        public TreeNode? root;
+
+        public IEnumerable<int> postOrderTraversal(TreeNode root)
+        {
+            if (root.left != null)
+            {
+                foreach (int i in postOrderTraversal(root.left!)) yield return i;
+            }
+            if (root.right != null)
+            {
+                foreach (int i in postOrderTraversal(root.right!)) yield return i;
+            }
+            yield return root.val;
+        }
+
+        public void createTestCase()
+        {
+            TreeNode nodePtr;
+            TreeNode root = new TreeNode(1);
+            this.root = root;
+            TreeNode newNode = new TreeNode(2);
+            this.root.right = newNode;
+            nodePtr = this.root.right;
+            newNode = new TreeNode(3);
+            nodePtr.left = newNode;
+            return;
+        }
+
+        public void createTestCase2()
+        {
+            TreeNode nodePtr;
+            TreeNode root = new TreeNode(1);
+            this.root = root;
+            
+            // Left Side of the Tree
+            TreeNode newNode = new TreeNode(2);
+            this.root.left = newNode;
+            nodePtr = this.root.left;
+            newNode = new TreeNode(4);
+            nodePtr.left = newNode;
+            newNode = new TreeNode(5);
+            nodePtr.right = newNode;
+            nodePtr = nodePtr.right;
+            newNode = new TreeNode(6);
+            nodePtr.left = newNode;
+            newNode = new TreeNode(7);
+            nodePtr.right = newNode;
+
+            // Right Side of the Tree
+            newNode = new TreeNode(3);
+            this.root.right = newNode;
+            nodePtr = this.root.right;
+            newNode = new TreeNode(8);
+            nodePtr.right = newNode;
+            newNode = new TreeNode(9);
+            nodePtr = nodePtr.right;
+            nodePtr.left = newNode;
+        }
+        public BST()
+        {
+            this.root = null;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // Test Case 1
+            BST bst1 = new BST();
+            bst1.createTestCase();
+            foreach (int i in bst1.postOrderTraversal(bst1.root!))
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
+            // Test Case 2
+            BST bst2 = new BST();
+            bst2.createTestCase2();
+            foreach (int i in bst1.postOrderTraversal(bst2.root!))
+                Console.Write($"{i} ");
+            Console.WriteLine();
+            return;
+        }
+    }
+}
---
+++ KLengthApart/KLengthApart.cs
@@ -0,0 +1,41 @@
+﻿// LeetCode 1437
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static readonly List<int> nums = new List<int>() { 1, 0, 0, 0, 1, 0, 0, 1 };
+    public static readonly int k = 2;
+    public static readonly List<int> nums1 = new List<int>() { 1, 0, 0, 1, 0, 1 };
+    public static readonly int k1 = 2;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public bool KLengthApart(List<int> nums, int k)
+        {
+            int distance = 0;
+            for (int i = 0; i<nums.Count; i++)
+            {
+                if (nums[i] == 1 && distance != 0)
+                {
+                    if (distance < k) return false; 
+                    distance = 0;
+                    continue;
+                }
+                distance++;
+            }
+            return true;
+        }
+
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.KLengthApart(Globals.nums, Globals.k));
+            Console.WriteLine(obj.KLengthApart(Globals.nums1, Globals.k1));
+            return;
+        }
+    }
+}
