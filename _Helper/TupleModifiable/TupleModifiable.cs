75f209861763668f33b566e7be566b32a7322f59
Vinton Sistoza <tsistoza@gmail.com>
2025-01-03 22:44:43 -1000

    Tuple except modifable
    Didnt really like that tuple is readonly decided to make my own version

---
+++ _Helper/TupleModifiable/TupleModifiable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Helpers
+{
+    public class Pair<T1, T2>
+    {
+        public T1? item1 { get; set; }
+        public T2? item2 { get; set; }
+
+        public Pair()
+        {
+            item1 = default;
+            item2 = default;
+        }
+
+        public Pair(T1 _item1, T2 _item2)
+        {
+            item1 = _item1;
+            item2 = _item2;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Pair<int, int> pair = new Pair<int, int>(1, 2);
+            Console.WriteLine(pair.item1);
+            Console.WriteLine(pair.item2);
+            return;
+        }
+    }
+}
+
