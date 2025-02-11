a5c7f6c4ea47b96d545cfde464f4830242d39266
Vinton Sistoza <tsistoza@gmail.com>
2025-02-11 11:45:23 -1000

    SmallestOccupiedChair.cs update
    fixed a case where there are multiple overlapping chairs, it would return the number of chairs instead of its smallest unoccupied chair

--- SmallestOccupiedChair/SmallestOccupiedChair.cs
+++ SmallestOccupiedChair/SmallestOccupiedChair.cs
@@ -12,22 +12,53 @@ public static class Globals
     public static int targetFriend2 = 8;
 }
 
+public class Triplet
+{
+    public int index;
+    public int start;
+    public int end;
+
+    public Triplet(int _index, int _start, int _end)
+    {
+        index = _index; start = _start; end = _end;
+    }
+}
+
+public class Compare : IComparer<Triplet>
+{
+    int IComparer<Triplet>.Compare(Triplet? x, Triplet? y)
+    {
+        if (x!.start < y!.start) return -1;
+        if (x.start > y.start) return 1;
+        return 0;
+    }
+}
+
 namespace Solution
 {
+    
     public class Program
     {
         public int SmallestChair(int[,] times, int targetFriend)
         {
-            Dictionary<int,int> line = new Dictionary<int,int>();
+            List<Triplet> newTimes = new List<Triplet>();
             for (int i=0; i<times.GetLength(0); i++)
+                newTimes.Add(new Triplet(i, times[i, 0], times[i, 1]));
+            Compare cmp = new Compare();
+            newTimes.Sort(cmp);
+
+            Dictionary<int,int> line = new Dictionary<int,int>();
+            for (int i=0; i<newTimes.Count; i++)
             {
-                for (int j = times[i,0]; j < times[i,1]; j++)
+                for (int j = newTimes[i].start; j < newTimes[i].end; j++)
                 {
                     if (line.ContainsKey(j)) line[j]++;
                     else line.Add(j, 1);
                 }
+                if (newTimes[i].index == targetFriend) return line[newTimes[i].start] - 1;
             }
-            return line[times[targetFriend,0]]-1;
+
+            return -1;
         }
         public static void Main()
         {
