61a75da7a239c7ec95c0e9ff314589cd31f9230c
Vinton Sistoza <tsistoza@gmail.com>
2025-03-10 12:41:56 -1000

    MinRecolorBlocks.cs 2379
    LeetCode 2379

---
+++ MinRecolorBlocks/MinRecolorBlocks.cs
@@ -0,0 +1,47 @@
+﻿// LeetCode 2379
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static string blocks = "WBWBBBW";
+    public static int k = 2;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public int minimumRecolors(string blocks, int k)
+        {
+            int differences = int.MaxValue;
+            for (int i = k-1; i < blocks.Length; i++)
+            {
+                int currDifferences = 0;
+                for (int j = i - k + 1; j <= i; j++)
+                {
+                    if (differences != int.MaxValue) break;
+                    if (blocks[j] == 'W') currDifferences++;
+                }
+
+                if (differences != int.MaxValue)
+                {
+                    currDifferences = differences;
+                    if (blocks[i] == 'W') currDifferences++;
+                    if (blocks[i - k] == 'W') currDifferences--;
+                }
+
+                differences = Math.Min(differences, currDifferences);
+                if (differences == 0) return differences;
+            }
+
+            return differences;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.minimumRecolors(Globals.blocks, Globals.k));
+            return;
+        }
+    }
+}
