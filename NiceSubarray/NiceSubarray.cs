0cbfffd1fc22111a4dbc0928fb241682b7d0973a
Vinton Sistoza <tsistoza@gmail.com>
2025-03-18 18:55:02 -1000

    NiceSubarray.cs 2401
    LeetCode 2401

---
+++ NiceSubarray/NiceSubarray.cs
@@ -0,0 +1,41 @@
+﻿// LeetCode 2401
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int[] nums = { 3, 1, 5, 11, 13 };
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public int LongestNiceSubarray(int[] nums)
+        {
+            int max = int.MinValue;
+            for (int i=0; i<nums.Length; i++)
+            {
+                List<int> subarray = new List<int>() { nums[i] };
+                for (int j=i+1; j<nums.Length; j++)
+                {
+                    int k = 0;
+                    for (; k<subarray.Count; k++)
+                        if ((subarray[k] & nums[j]) != 0) break;
+
+                    if (k == subarray.Count) subarray.Add(k);
+                    else break;
+                }
+                max = Math.Max(max, subarray.Count);
+            }
+
+            return max;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.LongestNiceSubarray(Globals.nums));
+            return;
+        }
+    }
+}
