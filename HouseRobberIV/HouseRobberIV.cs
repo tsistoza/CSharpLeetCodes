3fd1026618fd22380a9cb9bd4507a891e6b3130a
Vinton Sistoza <tsistoza@gmail.com>
2025-03-28 15:14:38 -1000

    HouseRobberIV.cs 2560
    LeetCode 2560

---
+++ HouseRobberIV/HouseRobberIV.cs
@@ -0,0 +1,51 @@
+﻿// LeetCode 2560
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int[] nums = { 2, 7, 9, 3, 1 };
+    public static int k = 2;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        
+        public int MinCapabiliity(int[] nums, int k)
+        {
+            int low = 1;
+            int high = nums[0];
+            for (int i=1; i<nums.Length; i++)
+                high = Math.Max(high, nums[i]);
+            
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                int housesRobbed = 0;
+
+                for (int i=0; i < nums.Length;)
+                {
+                    if (nums[i] <= mid)
+                    {
+                        housesRobbed++;
+                        i += 2;
+                        continue;
+                    }
+                    i++;
+                }
+
+                if (housesRobbed < k) low = mid + 1;
+                else high = mid;
+            }
+            return low;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.MinCapabiliity(Globals.nums, Globals.k));
+            return;
+        }
+    }
+}
