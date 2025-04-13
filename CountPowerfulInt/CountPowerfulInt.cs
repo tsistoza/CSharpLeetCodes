33d63a417511acf2abaf38e46b6a69d4a55d54d5
Vinton Sistoza <tsistoza@gmail.com>
2025-04-13 13:22:00 -1000

    CountPowerfullInt.cs 2999
    Big issues with last code while it does find powerful integers it does not find all of them. Decided to do some research and found about digit DP, here is my implementation

--- CountPowerfulInt/CountPowerfulInt.cs
+++ CountPowerfulInt/CountPowerfulInt.cs
@@ -4,38 +4,35 @@ using System.Collections.Generic;
 
 public static class Globals
 {
-    public static long start = 15;
-    public static long finish = 215;
-    public static int limit = 6;
-    public static string s = "10";
+    public static long start = 1000;
+    public static long finish = 2000;
+    public static int limit = 4;
+    public static string s = "3000";
 }
 
 namespace Solution
 {
     public class Program
     {
-        public long NumberOfPowerfulInt(long start, long finish, int limit, string s)
+        private long solve(string num, int n, long ub, long lb, int limit)
         {
             long count = 0;
-            long num = Convert.ToInt64(s);
-            if (num >= start && num <= finish)
-                count++;
-            int exponent = s.Length - 1;
-            while (num < finish)
-            {
-                int multiplicand = 1;
-                exponent++;
+            long strNum = Convert.ToInt64(num);
+            Console.WriteLine(strNum);
+            if (strNum <= ub && strNum >= lb) count++;
+            if (n < 0) return 0;
 
-                long temp = num;
-                while (multiplicand < limit + 1)
-                {
-                    temp = num + (long)Math.Pow(10, exponent) * multiplicand++;
-                    if (temp > finish ) break;
-                    count++;
-                }
-                num += (long)Math.Pow(10, exponent);
+            for (int i=1; i<limit+1; i++)
+            {
+                string newNum = Convert.ToString(i) + num;
+                count += solve(newNum, n - 1, ub, lb, limit);
             }
+
             return count;
+        }
+        public long NumberOfPowerfulInt(long start, long finish, int limit, string s)
+        {
+            return solve(s, finish.ToString().Length-s.Length, finish, start, limit);
         }
         public static void Main()
         {
