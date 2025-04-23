f03715e2301907b5f54a314f1470083ba881048e
Vinton Sistoza <tsistoza@gmail.com>
2025-04-23 13:20:52 -1000

    CountLargestGroup.cs 1399
    LeetCode 1399

---
+++ CountLargestGroup/CountLargestGroup.cs
@@ -0,0 +1,50 @@
+﻿// LeetCode 1399
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int n = 2;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        private int digitSum(int x)
+        {
+            int sum = 0;
+            while(x > 0)
+            {
+                sum += x % 10;
+                x /= 10;
+            }
+
+            if (sum >= 10) sum = digitSum(sum); 
+            return sum;
+        }
+        public int CountLargestGroup(int n)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            int maxSize = 0;
+            int number = 0;
+            for (int i=1; i<n+1; i++)
+            {
+                int sum = i;
+                if (i >= 10) sum = digitSum(i);
+                if (!count.TryAdd(digitSum(i), 1))
+                    count[sum]++;
+                if (count[sum] > maxSize) number = 1;
+                if (count[sum] == maxSize) number++;   
+                maxSize = Math.Max(maxSize, count[sum]);
+            }
+            return number;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.CountLargestGroup(Globals.n));
+            return;
+        }
+    }
+}
