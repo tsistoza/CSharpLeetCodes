b646b02722456919413ae7a31adde3c2b930be04
Vinton Sistoza <tsistoza@gmail.com>
2025-06-02 12:12:25 -1000

    Candy.cs 135
    LeetCode 135

---
+++ Candy/Candy.cs
@@ -0,0 +1,44 @@
+﻿// LeetCode 135
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int[] ratings = { 60, 80, 100, 100, 100, 100, 100 };
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public int Candy(int[] ratings)
+        {
+            int sum = 1, lastCandy = 1;
+            for (int i=1; i<ratings.Length; i++)
+            {
+                if (ratings[i - 1] == ratings[i])
+                {
+                    sum++;
+                    lastCandy = 1;
+                }
+                if (ratings[i - 1] < ratings[i])
+                {
+                    lastCandy++;
+                    sum += lastCandy;
+                }
+
+                if (ratings[i-1] > ratings[i] && lastCandy == 1)
+                    sum += i + 1;
+                if (ratings[i-1] > ratings[i] && lastCandy >= 2)
+                    sum += lastCandy-- - 1;
+            }
+            return sum;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.Candy(Globals.ratings));
+            return;
+        }
+    }
+}
