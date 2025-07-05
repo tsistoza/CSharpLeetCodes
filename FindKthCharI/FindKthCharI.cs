a347c16b311b513d066155bfe0341e0ac391cb23
Vinton Sistoza <tsistoza@gmail.com>
2025-07-05 09:59:20 -1000

    FindKthCharI.cs 3304
    LeetCode 3304

---
+++ FindKthCharI/FindKthCharI.cs
@@ -0,0 +1,34 @@
+﻿// LeetCode 3304
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int k = 10;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public char KthCharacter(int k)
+        {
+            string word = "a";
+            while (word.Length < k)
+            {
+                string append = "";
+                foreach (char c in word) append += (c + 1 > 'z') ? 'a' : (char) (c + 1);
+                word += append;
+            }
+            Console.WriteLine(word);
+            return word[k];
+        }
+
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.KthCharacter(Globals.k));
+            return;
+        }
+    }
+}
