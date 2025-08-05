7a22ff8e76adac43a02a30de19f9ef08fbf686ae
Vinton Sistoza <tsistoza@gmail.com>
2025-08-05 12:54:13 -1000

    FruitBasketI.cs 904
    LeetCode 904

---
+++ FruitBasketI/FruitBasketI.cs
@@ -0,0 +1,59 @@
+﻿// LeetCode 904
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int[] fruits = { 1, 2, 3, 2, 2 };
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public int TotalFruit(int[] fruits)
+        {
+            int numFruits = 0;
+            Stack<int> idx = new Stack<int>();
+            Dictionary<int, int> baskets = new Dictionary<int, int>();
+            for (int i=0, window=0, currFruits=0; i+window<fruits.Length;)
+            {
+                if (baskets.ContainsKey(fruits[i+window]))
+                {
+                    baskets[fruits[i + window]]++;
+                } else
+                {
+                    idx.Push(i+window);
+                    baskets.Add(fruits[i + window], 1);
+                }
+
+                currFruits++;
+                window++;
+
+                if (i+window >= fruits.Length)
+                {
+                    numFruits = Math.Max(currFruits, numFruits);
+                    continue;
+                }
+
+                if (baskets.Count > 2)
+                {
+                    baskets.Clear();
+                    idx.Pop();
+                    i = idx.Peek();
+                    baskets.Add(fruits[i], 1);
+                    window = 1;
+                    numFruits = Math.Max(currFruits-1, numFruits);
+                    currFruits = 1;
+                }
+            }
+            return numFruits;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.TotalFruit(Globals.fruits));
+            return;
+        }
+    }
+}
