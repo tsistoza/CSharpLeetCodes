6aba93bf1e8d5bba01b5d18ef16d5dc7805ad671
Vinton Sistoza <tsistoza@gmail.com>
2025-08-30 16:05:43 -1000

    UniqueBST.cs 96
    LeetCode 96

---
+++ UniqueBST/UniqueBST.cs
@@ -0,0 +1,42 @@
+﻿// LeetCode 96
+using System;
+using System.Collections.Generic;
+
+public static class Globals
+{
+    public static int n = 5;
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public int NumTrees(int n)
+        {
+            int[] dp = new int[n+1];
+            dp[0] = 1;
+            dp[1] = 1;
+            if (n == 1) return dp[1];
+
+            for (int i=2; i<dp.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j=1; j<=i; j++)
+                {
+                    int left = j - 1;
+                    int right = i - 1 - left;
+                    sum += dp[left] * dp[right];
+                }
+                dp[i] = sum;
+            }
+
+            return dp[n];
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.NumTrees(Globals.n));
+            return;
+        }
+    }
+}
