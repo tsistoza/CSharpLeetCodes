f9f1800b9042197fae5c11e781fa505801a4b3d4
Vinton Sistoza <tsistoza@gmail.com>
2025-10-28 10:38:57 -1000

    ZeroArray.cs 3354
    LeetCode 3354

---
+++ ZeroArray/ZeroArray.cs
@@ -0,0 +1,41 @@
+﻿// LeetCode 3354
+using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public static class Globals
+    {
+        public static int[] nums = { 0 };
+    }
+
+    public class Program
+    {
+        public int CountValidSelections(int[] nums)
+        {
+            int sumRight = 0;
+            for (int i=0; i<nums.Length; i++)
+                sumRight += nums[i];
+
+            int sumLeft = 0, selections = 0;
+            for (int i=0; i<nums.Length; i++)
+            {
+                sumLeft += nums[i];
+                sumRight -= nums[i];
+
+                int difference = (int)MathF.Abs(sumRight - sumLeft);
+                if (nums[i] == 0 && difference == 0)
+                    selections += 2;
+                if (nums[i] == 0 && difference == 1)
+                    selections++;
+            }
+            return selections;
+        }
+        public static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.CountValidSelections(Globals.nums));
+            return;
+        }
+    }
+}
