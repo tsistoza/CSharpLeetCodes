0543237d65011a70f2bb23a80848a237c4cd206f
Vinton Sistoza <tsistoza@gmail.com>
2026-01-02 09:44:54 -1000

    NRepeatedElem.cs 961

---
+++ Entry/Problems/NRepeatedElem.cs
@@ -0,0 +1,31 @@
+﻿// LeetCode 961
+using System;
+using System.Collections.Generic;
+
+namespace _961
+{
+    public static class Globals
+    {
+        public static int[] nums = { 2, 1, 2, 5, 3, 2 };
+    }
+    public class Program
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            int n = nums.Length / 2;
+            int repeatedElem = -1;
+            Dictionary<int, int> nRepeated = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!nRepeated.TryAdd(num, 1))
+                    nRepeated[num]++;
+                if (nRepeated[num] == n)
+                {
+                    repeatedElem = num;
+                    break;
+                }
+            }
+            return repeatedElem;
+        }
+    }
+}
