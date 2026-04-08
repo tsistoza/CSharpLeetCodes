1b0b68ff0519b0b058ac92b687ce0fae37a3ed68
Vinton Sistoza <tsistoza@gmail.com>
2026-04-08 12:05:49 -1000

    TribonacciNumber.cs 1137

---
+++ Entry/Problems/TribonacciNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1137
+{
+    public static class Globals
+    {
+        public static int n = 25;
+    }
+    public class Program
+    {
+        public int Tribonacci(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i=0; i+3<n+1; i++) dp[i + 3] = dp[i] + dp[i + 1] + dp[i + 2];
+            return dp[n];
+        }
+    }
+}
